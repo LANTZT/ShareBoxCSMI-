f01c1065bcd41283d9d59af867655ccd8f1c07fb
LANTZT <tlantz35@yahoo.com>
2014-09-30 20:04:05 +0200

    Stable version 1.1

--- EF/membrane.cpp
+++ EF/membrane.cpp
@@ -10,47 +10,61 @@ int main( int argc, char** argv ) {
   ( "R", po::value<double>()->default_value(0.3), "Radius" )
   ( "theta", po::value<double>()->default_value(0), "Angle" )
   ( "A", po::value<double>()->default_value(1.0), "Amplitude" );
+ 
   Environment env( _argc=argc, _argv=argv,
                    _desc=options,
                    _about=about(_name="membrane",
-                                _author="Feel++ Consortium",
-                                _email="feelpp-devel@feelpp.org"));
+                                _author="CSMI"
+                               ));
   auto mesh = unitCircle();
-  auto A=option(_name="A").as<double>();
-  auto R=option(_name="R").as<double>();
-  auto T=option(_name="T").as<double>();
-  auto x0=option(_name="x0").as<double>();
-  auto y0=option(_name="y0").as<double>();
-  auto theta=option(_name="theta").as<double>();
-  auto sigma=option(_name="sigma").as<double>();
-    // $\mathbb{P}_{2}$ finite element space on
+  // $\mathbb{P}_{2}$ finite element space on
   // triangular elements
   auto Xh = Pch<2>(mesh);
   auto u = Xh->element();
   auto v = Xh->element();
-  //auto ue = expr("1+x*x+2*y*y:x:y");
-  //auto f = expr("-6:x:y");
-    auto f=expr("4*exp(-0.5*(pow((R*x-x0)/sigma,2)) - 0.5*(pow((R*y-y0)/sigma,2))):x:y:R:T:sigma:x0:y0");
-  // $\int_\Omega f v$
-  auto l = form1(_test=Xh);
-  l= integrate( _range=elements(mesh),_expr= f*id(v) );
-    // $\int_\Omega \nabla u \cdot \nabla v$
+//  auto ue = expr("1+x*x+2*y*y:x:y");
+//  auto f = expr("-6:x:y");
+  auto f=expr("4*exp(-0.5*(pow((R*x-x0)/sigma,2)) - 0.5*(pow((R*y-y0)/sigma,2))):x:y:R:T:sigma:x0:y0"); 
+
+  double T = option(_name="T").as<double>();
+  double x0 = option(_name="x0").as<double>(); 
+  double y0  = option(_name="y0").as<double>();
+  double sigma = option(_name="sigma").as<double>();
+  double R = option(_name="R").as<double>();
+  double theta = option(_name="theta").as<double>();
+  double A = option(_name="A").as<double>();
+
+ auto F=vf::project( _space=Xh, _range=elements(mesh), _expr=4*exp(-0.5*(pow((R*Px()-x0)/sigma,2)) - 0.5*(pow((R*Py()-y0)/sigma,2))));
+
+
+  // $\int_\Omega \nabla u \cdot \nabla v$
   auto a = form2(_test=Xh,_trial=Xh);
   a = integrate( _range=elements(mesh),
                  _expr=gradt(u)*trans(grad(v)) );
-  a+=on( _range=boundaryfaces(mesh),_element= u, _rhs=l, _expr=cst(0.) );
-   // solve a( u, v ) = l( v )
+  
+  // $\int_\Omega f v$
+  auto l = form1(_test=Xh);
+  l= integrate(_range=elements(mesh),
+               _expr= f*id(v) );
+
+//  a+=on( boundaryfaces(mesh), u, l, ue );
+  
+  a+=on(_range=boundaryfaces(mesh), _rhs=l, _element=u, _expr=cst(0.));
+
+  // solve a( u, v ) = l( v )
   a.solve( _solution=u, _rhs=l );
 
-  std::cout << "|u-ue|= "
-            << normL2(_range=elements(mesh),
-                      _expr=(idv(u)-ue) )
-            << "\n";
+//  std::cout << "|u-ue|= "
+//            << normL2(_range=elements(mesh),
+//                      _expr=(idv(u)-ue) )
+//            << "\n";
+  
 
   auto e = exporter( _mesh=mesh ); 
   e->add( "u", u );
+  e->add("f", F);
   // v interpolate ue
-  v.on(_range=elements(mesh),_expr=ue );
-  e->add( "ue", v );
+ // v.on(_range=elements(mesh),_expr=ue );
+ // e->add( "ue", v );
   e->save();
 }
